e78d310f519ad0bb53ecc3c7eef8595988426f4d
Sam Uherek <samuherekbiz@gmail.com>
2016-06-04 19:57:33 +0200

    forgot the routes without admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    DashboardManifest::DASHBOARDS.each do |dashboard_resource|
-      resources dashboard_resource
-    end
-
-    root controller: DashboardManifest::ROOT_DASHBOARD, action: :index
-  end
-
   devise_for :users
   resources :episodes
   resource :subscription
-  
+
   root to: "episodes#index"
 
 end
